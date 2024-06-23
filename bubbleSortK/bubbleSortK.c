efcf405eb450d51b14098c84759b3602c46ab332
antogit-sys <antonioperrgiorgi@gmail.com>
2024-06-23 18:30:51 +0200

    fix k

--- bubbleSortK/bubbleSortK.c
+++ bubbleSortK/bubbleSortK.c
@@ -5,7 +5,7 @@ int* bubbleSortK(int* arr, size_t dim, short k){
     int* carray = NEW_ARRAY(int, dim);
     ARRCPY(carray, arr, dim);
 
-    /* Descending order  (<)*/
+    /* k = 1 (Crescent) || k = -1 (Decrescent*/
     for(int i = 0; i < dim-1; ++i){
         for(int j = 0; j < dim-i-1; ++j){
             if( (k * (carray[j]-carray[j+1])) > 0){
