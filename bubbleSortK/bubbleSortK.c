54476ed837741288409a87f85c1f1c0626e0b08d
antogit-sys <antonioperrgiorgi@gmail.com>
2024-06-23 18:15:34 +0200

    Bubble Sort K

---
+++ bubbleSortK/bubbleSortK.c
@@ -0,0 +1,18 @@
+#include "../header/sorting.h"
+
+int* bubbleSortK(int* arr, size_t dim, short k){
+    /* the function operate to copy, not original array */
+    int* carray = NEW_ARRAY(int, dim);
+    ARRCPY(carray, arr, dim);
+
+    /* Descending order  (<)*/
+    for(int i = 0; i < dim-1; ++i){
+        for(int j = 0; j < dim-i-1; ++j){
+            if( (k * (carray[j]-carray[j+1])) > 0){
+                SWAP_VALUE(carray[j], carray[j+1]);
+            }
+        }
+    }
+
+    return carray;
+}
