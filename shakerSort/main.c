fb007b7e6efb32531bb57a3b332ba5810a134913
antogit-sys <antonioperrgiorgi@gmail.com>
2024-06-26 16:16:46 +0200

    fix main shaker

--- shakerSort/main.c
+++ shakerSort/main.c
@@ -5,22 +5,16 @@
 
 int main(void)
 {
-   int ordinateDecrescent[N];
-   //int ordinateCrescent[N];
-
-   ARRCPY(ordinateDecrescent, shakerSort(EXAMPLE_ARRAY, N), N);
-   //ARRCPY(ordinateCrescent, bubbleSortK(EXAMPLE_ARRAY, N, 1), N);
+   int array[N];
    
+   ARRCPY(array, shakerSort(EXAMPLE_ARRAY, N), N);
+      
    puts("generic array: ");
    ARROUT(EXAMPLE_ARRAY, N);
    putchar('\n');
 
-   //puts("Crescent ordinate array (k = 1): ");
-   //ARROUT(ordinateCrescent, N);
-   //putchar('\n');
-
-   puts("Decrescent ordinate array (k = -1): ");
-   ARROUT(ordinateDecrescent, N);
+   puts("ordinate array: ");
+   ARROUT(array, N);
    putchar('\n');
 
 return 0;
