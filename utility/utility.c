a206f7d1bd047e0598acccd4e3a8486d70f7c499
antogit-sys <antonioperrgiorgi@gmail.com>
2024-06-27 12:42:31 +0200

    fix found_max

--- utility/utility.c
+++ utility/utility.c
@@ -1,9 +1,10 @@
+#include "../header/sorting.h"
 
-int foubnd_max(int* arr, size_t dim){
+int found_max(int* arr, size_t dim){
     int max = arr[0];
 
     for(size_t i = 1; i < dim; ++i){
-        if(arr[i] > arr[i+1])
+        if(arr[i] > max)
             max = arr[i];
     }
     return max;
