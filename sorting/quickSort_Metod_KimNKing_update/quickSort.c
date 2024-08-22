bc01533745c03700c7b24b88b38d3ff629a2ccfc
antogit-sys <antonioperrgiorgi@gmail.com>
2024-08-22 13:28:39 +0200

    add comment quick sort kim n king

--- sorting/quickSort_Metod_KimNKing_update/quickSort.c
+++ sorting/quickSort_Metod_KimNKing_update/quickSort.c
@@ -26,14 +26,26 @@ long __splitk(int* arr, long l, long h){
     long part_element = arr[l];
 
     for(;;){
+        /*
+         * Riassunto della dinamica:
+         *  - Da destra a sinistra: La funzione cerca un elemento che 
+         *    sia più piccolo del pivot e lo sposta a sinistra.
+         *
+         *  - Da sinistra a destra: La funzione cerca un elemento che 
+         *    sia più grande del pivot e lo sposta a destra.
+         */
+
+        // SCORRE DA SX A DX
         while(l < h && part_element <= arr[h]) h--;
         if(l >= h) break;  
-        arr[l++] = arr[h];
+        arr[l++] = arr[h]; //Se l è ancora inferiore a h, l'elemento a destra dell'array (
+                           //cioè arr[h]) viene copiato nella posizione di arr[l].
 
+        //SCORRE DA DX A SX
         while(l < h && arr[l] <= part_element) l++;
-        if(l >= h) break;
-        arr[h--] = arr[l];
+        if(l >= h) break; 
+        arr[h--] = arr[l]; 
     }
-    arr[h] = part_element;
+    arr[h] = part_element; 
     return h;
 }
